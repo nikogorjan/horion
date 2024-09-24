6ceba7a9f4e91ec41bb37b7b5f1f7c988ce6a000
Niko Gorjan <niko.gorjan@gmail.com>
2024-09-24 13:22:56 +0200

    pais

--- src/app/novice/herpes/page.js
+++ src/app/novice/herpes/page.js
@@ -1,34 +1,39 @@
 import Footer from "../../../../components/Footer/Footer";
 import GoogleAnalytics from "../../../../components/GoogleAnalytics/GoogleAnalytics";
 import Navbar from "../../../../components/Navbar/Navbar";
-import Herpes from "../../../../components/Novice/Herpes/Herpes"
-import Head from 'next/head';
+import Herpes from "../../../../components/Novice/Herpes/Herpes";
+
+export const metadata = {
+  title: "Genitalni Herpes: Vse, kar morate vedeti o simptomih, zdravljenju in preventivi",
+  description: "Raziskali bomo vse vidike genitalnega herpesa, od simptomov do zdravljenja in preventivnih ukrepov.",
+  openGraph: {
+    type: 'article',
+    url: "https://horion.si/novice/herpes",
+    title: "Genitalni Herpes: Vse, kar morate vedeti o simptomih, zdravljenju in preventivi",
+    description: "Raziskali bomo vse vidike genitalnega herpesa, od simptomov do zdravljenja in preventivnih ukrepov.",
+    images: [
+      {
+        url: "/Images/novica11.webp",
+        alt: "Genitalni Herpes Article Image",
+      }
+    ],
+    siteName: 'Horion',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Genitalni Herpes: Vse, kar morate vedeti o simptomih, zdravljenju in preventivi",
+    description: "Raziskali bomo vse vidike genitalnega herpesa, od simptomov do zdravljenja in preventivnih ukrepov.",
+    images: ["/Images/novica11.webp"],
+  }
+};
 
 export default function Dokumentacija() {
-    const postTitle = "Genitalni Herpes: Vse, kar morate vedeti o simptomih, zdravljenju in preventivi";
-    const postDescription = "Raziskali bomo vse vidike genitalnega herpesa, od simptomov do zdravljenja in preventivnih ukrepov.";
-    const postImage = "/Images/novica11.webp"; // The path to the image to display when shared
-    const postUrl = "https://horion.si/novice/herpes"; // Your blog post URL
-
-
-    return (
-        <main>
-            <Head>
-        {/* Open Graph meta tags for Facebook */}
-        <title>{postTitle}</title>
-        <meta name="description" content={postDescription} />
-
-        <meta property="og:title" content={postTitle} />
-        <meta property="og:description" content={postDescription} />
-        <meta property="og:url" content={postUrl} />
-        <meta property="og:type" content="article" />
-        <meta property="og:image" content={postImage} />
-        <meta property="og:site_name" content="Horion" />
-      </Head>
-            <Navbar />
-            <GoogleAnalytics />
-            <Herpes />
-            <Footer />
-        </main>
-    );
+  return (
+    <main>
+      <Navbar />
+      <GoogleAnalytics />
+      <Herpes />
+      <Footer />
+    </main>
+  );
 }
